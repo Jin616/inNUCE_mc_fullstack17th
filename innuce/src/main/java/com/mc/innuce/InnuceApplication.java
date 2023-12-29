9d7fdc80df850f393ef68457b6a37071082078b9
jingseo <coding0616@gmail.com>
2023-12-29 15:43:05 +0900

    first setting

---
+++ innuce/src/main/java/com/mc/innuce/InnuceApplication.java
@@ -0,0 +1,13 @@
+package com.mc.innuce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnuceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnuceApplication.class, args);
+	}
+
+}
